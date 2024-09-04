99771e1a0b0c04881b5c6dcf3f1e66685589f1e0
Nicklaus Thompson <93845033+FangameEmpire@users.noreply.github.com>
2024-09-03 23:16:58 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_NicklausThompson_SkyKing (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -60,4 +60,4 @@ module tt_um_example (
     end
   end
 
-endmodule
+endmodule // tt_um_NicklausThompson_SkyKing
